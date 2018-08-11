eaab5060e7188150b84e22380c69ab941d5967bf
Yoshihiro Tanaka <contact@cordea.jp>
2018-08-11 16:44:10 +0900

    add pause controller

---
+++ PauseController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cordea
+ * Date: 2018/08/11
+ * Time: 16:45
+ */
+
+require 'vendor/autoload.php';
+require 'IController.php';
+
+class PauseController implements IController
+{
+    private $api;
+
+    public function __construct(\SpotifyWebAPI\SpotifyWebAPI $api)
+    {
+        $this->api = $api;
+    }
+
+    public function call()
+    {
+        $this->api->pause();
+    }
+}
