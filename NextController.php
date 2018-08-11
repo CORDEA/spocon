7e242b1e90addfff6a1eee0c566be56f7b7c4a11
Yoshihiro Tanaka <contact@cordea.jp>
2018-08-11 16:47:22 +0900

    add next controller

---
+++ NextController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cordea
+ * Date: 2018/08/11
+ * Time: 16:46
+ */
+
+require 'vendor/autoload.php';
+require 'IController.php';
+
+class NextController implements IController
+{
+    private $api;
+
+    public function __construct(\SpotifyWebAPI\SpotifyWebAPI $api)
+    {
+        $this->api = $api;
+    }
+
+    public function call()
+    {
+        $this->api->next();
+    }
+}
